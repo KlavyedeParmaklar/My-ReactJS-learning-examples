e69b47702a36de6dbcd6c0b64b91a180016206dd
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-08-11 16:42:31 +0100

    Playing with "State"

--- expense-tracker/src/components/Expense/ExpenseItem.js
+++ expense-tracker/src/components/Expense/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
@@ -5,15 +7,18 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+    const [title, setTitle] = useState(props.title)
+
     const clickHandler = () => {
-        alert("OMG!! You Clicked!")
+        setTitle('Updated!')
+        alert(title)
     }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>£{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
