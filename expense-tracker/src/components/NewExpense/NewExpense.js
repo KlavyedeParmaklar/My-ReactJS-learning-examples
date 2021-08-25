c1a2ddf730d051fb33a83941fda32e5592c2e473
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-08-25 09:20:27 +0100

    Modal for New Expense

--- expense-tracker/src/components/NewExpense/NewExpense.js
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ExpenseForm from './ExpenseForm'
 
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+
+    const [openExpenseForm, setOpenExpenseForm] = useState(false)
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -14,9 +16,23 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData)
     }
 
+    const openNewExpenseFormHandler = () => {
+        setOpenExpenseForm(true)
+    }
+
+    const closeNewExpenseFormHandler = () => {
+        setOpenExpenseForm(false)
+    }
+
+    let newExpenseForm = <button onClick={openNewExpenseFormHandler}>Add New Expense</button>
+
+    if (openExpenseForm) {
+        newExpenseForm = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={closeNewExpenseFormHandler} />
+    }
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {newExpenseForm}
         </div>
     )
 }
