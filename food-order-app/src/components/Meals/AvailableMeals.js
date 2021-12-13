66882bb2cf6104b213a6b57a4e285032df16fbd1
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-12-13 22:02:09 +0000

    Meal Item JS & CSS

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import MealItem from './MealItem/MealItem';
+
+import Card from '../UI/Card/Card'
 
 import classes from './AvailableMeals.module.css'
 
@@ -30,13 +33,21 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealsList = DUMMY_MEALS.map(meal =>
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />)
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
