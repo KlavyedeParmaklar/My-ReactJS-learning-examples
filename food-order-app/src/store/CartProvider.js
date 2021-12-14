4c1afe3834b00865615b1baa41ca50578d1bbaa2
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-12-14 13:47:27 +0000

    Cart Context & Global State

---
+++ food-order-app/src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import CartContext from "./cart-content"
+
+const CartProvider = (props) => {
+
+    const addItemToCartHandler = item => { }
+
+    const removeItemFromCartHandler = id => { }
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        remoteItem: removeItemFromCartHandler
+    }
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
