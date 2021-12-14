5ecb49be652e449fdad91fcabc98f775bbffa9da
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-12-14 21:04:49 +0000

    Full Functionalty for Cart

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import MealItemForm from './MealItemForm'
+import CartContext from '../../../store/cart-content'
 
 import classes from './MealItem.module.css'
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+    const cartCtx = useContext(CartContext)
+
+    const price = `£${props.price.toFixed(2)}`
+
+    const addItemToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
@@ -14,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={addItemToCartHandler} />
             </div>
         </li>
     )
