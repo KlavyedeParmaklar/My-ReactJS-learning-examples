e7b3061e8d93a2bd55adce6307f1a1bad9e9bc74
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-12-27 15:30:25 +0000

    Handling Error

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -8,10 +8,16 @@ import classes from './AvailableMeals.module.css'
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [httpError, setHttpError] = useState(null)
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch('https://react-http-6be27-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!F')
+            }
+
             const responseData = await response.json()
 
             const loadedMeals = []
@@ -28,13 +34,24 @@ const AvailableMeals = () => {
             setMeals(loadedMeals)
             setIsLoading(false)
         }
-        fetchMeals()
+        fetchMeals().catch((error) => {
+            setIsLoading(false)
+            setHttpError(error.message)
+        })
     }, [])
 
     if (isLoading) {
         return (
             <section className={classes.MealsLoading}>
                 <p>Loading...</p>
+            </section>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
             </section>
         )
     }
