3d265ec982d9689ef748f6320efd7b8790a72f5d
KlavyedeParmaklar <sepco.kilic@hotmail.com>
2021-12-27 22:49:40 +0000

    Adding Form Validation

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -1,8 +1,18 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim() === ''
+const isFiveChars = (value) => value.trim().length === 5
+
 const Checkout = (props) => {
+    const [forminputsValidity, setFormInputsValidity] = useState({
+        name: true,
+        street: true,
+        city: true,
+        postalCode: true,
+    })
+
     const nameInputRef = useRef()
     const streetInputRef = useRef()
     const postalCodeInputRef = useRef()
@@ -15,25 +25,52 @@ const Checkout = (props) => {
         const enteredStreet = streetInputRef.current.value
         const enteredPostalCode = postalCodeInputRef.current.value
         const enteredCity = cityInputRef.current.value
+
+        const enteredNameIsValid = !isEmpty(enteredName)
+        const enteredStreetIsValid = !isEmpty(enteredStreet)
+        const enteredCityIsValid = !isEmpty(enteredCity)
+        const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode)
+
+        setFormInputsValidity({
+            name: enteredNameIsValid,
+            street: enteredStreetIsValid,
+            city: enteredCityIsValid,
+            postalCode: enteredPostalCodeIsValid,
+        })
+
+        const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredPostalCodeIsValid && enteredCityIsValid
+
+        if (!formIsValid) {
+            return
+        }
     };
+
+    const nameControlClasses = `${classes.control} ${forminputsValidity.name ? '' : classes.invalid}`
+    const streetControlClasses = `${classes.control} ${forminputsValidity.street ? '' : classes.invalid}`
+    const cityControlClasses = `${classes.control} ${forminputsValidity.city ? '' : classes.invalid}`
+    const postalControlClasses = `${classes.control} ${forminputsValidity.postalCode ? '' : classes.invalid}`
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
-            <div className={classes.control}>
+            <div className={nameControlClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input type='text' id='name' ref={nameInputRef} />
+                {!forminputsValidity.name && <p>Please enter a valid name!</p>}
             </div>
-            <div className={classes.control}>
+            <div className={streetControlClasses}>
                 <label htmlFor='street'>Street</label>
                 <input type='text' id='street' ref={streetInputRef} />
+                {!forminputsValidity.street && <p>Please enter a valid street!</p>}
             </div>
-            <div className={classes.control}>
+            <div className={postalControlClasses}>
                 <label htmlFor='postal'>Postal Code</label>
                 <input type='text' id='postal' ref={postalCodeInputRef} />
+                {!forminputsValidity.postalCode && <p>Please enter a valid Postal Code!</p>}
             </div>
-            <div className={classes.control}>
+            <div className={cityControlClasses}>
                 <label htmlFor='city'>City</label>
                 <input type='text' id='city' ref={cityInputRef} />
+                {!forminputsValidity.city && <p>Please enter a valid city!</p>}
             </div>
             <div className={classes.actions}>
                 <button type='button' onClick={props.onCancel}>
